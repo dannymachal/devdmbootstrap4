f64e02b403a636babb50917ab7fe1e9ea5d26703
Danny Machal <danny@devdm.com>
2015-10-05 22:17:06 +0700

    setting up file structure

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 9/14/2015
+ * Time: 5:31 PM
+ */
---
+++ singular.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 10/4/2015
+ * Time: 4:12 PM
+ */
