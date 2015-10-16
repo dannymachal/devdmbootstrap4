a5fc7afc09d979ba8513cd77d9a8cb736613cabc
Danny Machal <danny@devdm.com>
2015-10-16 11:53:25 +0700

    nav, css

--- template-parts/head.php
+++ template-parts/head.php
@@ -3,10 +3,17 @@
     <div class="row">
 
         <div class="col-sm-4 dmbs-header-left">
-            <img src="http://placehold.it/350x150">
+
+            <a class="dmbs-logo-link" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <img class="dmbs-logo-image" src="<?php header_image(); ?>" alt="<?php bloginfo('name'); ?>" />
+            </a>
+
         </div>
 
         <div class="col-sm-8 dmbs-header-right">
+
+            <h1 class="dmbs-header-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+            <h4 class="dmbs-header-description"><?php bloginfo( 'description' ); ?></h4>
 
         </div>
 
