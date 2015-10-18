2d8824c14ec3ef023344ea0d98e91eedb270f99f
Danny Machal <danny@devdm.com>
2015-10-18 20:59:06 +0700

    comment work

--- single.php
+++ single.php
@@ -1,7 +1,75 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ADMIN
- * Date: 9/14/2015
- * Time: 5:27 PM
- */
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/head'); ?>
+
+<?php get_template_part('template-parts/nav','header'); ?>
+
+    <div class="container dmbs-content-wrapper">
+        <div class="row">
+
+            <?php get_template_part( 'template-parts/sidebar', 'left' ); ?>
+
+            <div class="col-sm-<?php echo devdmbootstrap_column_size('main'); ?> dmbs-main">
+
+                <?php if ( have_posts() ) : ?>
+
+                    <?php
+                    // Start the loop.
+                    while ( have_posts() ) : the_post(); ?>
+
+                        <article id="post-<?php the_ID(); ?>" <?php post_class('card dmbs-post'); ?>>
+
+                            <header class="card-header dmbs-post-header">
+                                <?php the_title( sprintf( '<h2 class="dmbs-post-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                                <?php get_template_part('template-parts/postmeta','header'); ?>
+                            </header>
+
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <div class="dmbs-post-featured-image">
+                                    <?php the_post_thumbnail(); ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <div class="card-block dmbs-post-content">
+
+                                <?php if ( has_excerpt() ) : ?>
+                                    <div class="dmbs-post-summary">
+                                        <?php the_excerpt(); ?>
+                                    </div><!-- .entry-summary -->
+                                <?php endif; ?>
+
+                                <?php
+                                the_content( sprintf(
+                                    wp_kses( esc_html__( 'Continue reading %s', 'devdmbootstrap4' ), array( 'span' => array( 'class' => array() ) ) ),
+                                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                                ) );
+                                ?>
+
+                            </div>
+
+                            <footer class="card-footer dmbs-post-footer">
+                                <?php get_template_part('template-parts/postmeta','footer'); ?>
+                            </footer>
+
+                        </article>
+
+                        <?php comments_template( '/template-parts/comments.php'); ?>
+
+                    <?php
+                        // End the loop.
+                    endwhile;
+                    ?>
+
+                <?php else :  ?>
+
+                    <?php get_404_template(); ?>
+
+                <?php endif; ?>
+            </div>
+
+            <?php get_template_part( 'template-parts/sidebar', 'right' ); ?>
+
+        </div>
+    </div>
+
+<?php get_footer(); ?>
---
+++ template-parts/comments.php
@@ -0,0 +1,38 @@
+<?php if ( is_single() || is_page() && comments_open() ) : ?>
+
+    <div class="dmbs-comments">
+
+        <a name="comments"></a>
+
+        <?php if ( have_comments() ) : ?>
+
+            <h4 id="comments">
+                <span class="fa fa-comment"></span>
+                <?php comments_number('0 ' . __('Comments','devdmbootstrap4'), '1 ' . __('Comment','devdmbootstrap4'), '%' . __(' Comments','devdmbootstrap4') );?>
+            </h4>
+
+            <ul class="list-unstyled">
+
+                <?php
+                // Register Custom Comment Walker
+                require_once(__DIR__ . '/../includes/class-wp-bootstrap-comment-walker.php');
+
+                wp_list_comments( array(
+                    'style'         => 'ul',
+                    'short_ping'    => true,
+                    'avatar_size'   => '64',
+                    'walker'        => new Bootstrap_Comment_Walker(),
+                ) );
+                ?>
+
+                <?php paginate_comments_links(); ?>
+                <?php wp_enqueue_script( "comment-reply" ); ?>
+            </ul>
+
+        <?php endif; ?>
+
+        <?php comment_form(); ?>
+
+    </div>
+
+<?php endif; ?>
